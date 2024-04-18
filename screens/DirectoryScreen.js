74c245da210a998d0bad5257b01150449942f2a4
Hannah <121524600+hdenneny@users.noreply.github.com>
2024-04-17 22:08:45 -0700

    Activity indicator

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,10 +1,25 @@
-import { FlatList } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseURL } from '../shared/baseURL';
+import Loading from '../components/LoadingComponent';
 
 const DirectoryScreen = ({ navigation }) => {
     const campsites = useSelector((state) => state.campsites);
+
+    if (campsites.isLoading){
+        return <Loading />
+    }
+
+    if(campsites.errMess){
+        return(
+            <View>
+                <Text>
+                    {campsites.errMess}
+                </Text>
+            </View>
+        )
+    }
 
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
